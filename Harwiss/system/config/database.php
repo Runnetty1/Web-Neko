affa1f50615383b953793f5f5115ee6c27db966f
Mats Harwiss <rcdriftnitro2@gmail.com>
2019-04-09 14:37:28 +0200

    Update database.php
    added helfull info

--- Harwiss/system/config/database.php
+++ Harwiss/system/config/database.php
@@ -1,6 +1,9 @@
 <?php
 class database{
-	
+	/*
+		The data here is just for use in the dev environment.
+		Make sure to never upload your acctual Database connection data publicly.
+	*/
 	public $host = "localhost";
 	public $port = 3307;
 	public $username = "root";
@@ -10,9 +13,5 @@ class database{
 	function getDb(){
 		return  "mysql:host=$this->host;port=$this->port;dbname=$this->db_name;charset=utf8";
 	}
-	
 }
-	
-	
-	
 ?>
